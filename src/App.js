edf5d67e6995295bd247858a830c13d966b4f419
fabio-sv <fabio_dev@outlook.com>
2022-08-03 21:40:15 -0300

    criado a lógica de estagios do jogo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import { wordsList } from "./data/words";
 import "./App.css";
 import SecretWord from "./components/SecretWord";
 
+const stages = [
+    { id: 1, name: "start" },
+    { id: 2, name: "game" },
+    { id: 3, name: "end" },
+];
+
 function App() {
-    return <div className="App">
-        <SecretWord />
-    </div>;
+    const [gameStage, setGameStage] = useState(stages[0].name);
+    const [words] = useState(wordsList);
+
+    return (
+        <div className="App">
+            <SecretWord />
+        </div>
+    );
 }
 
 export default App;
